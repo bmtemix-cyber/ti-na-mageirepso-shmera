eb150a9982cc1edbb1d859d9bf9c9b809711bb96
bmtemix-cyber <bmtemix@hotmail.com>
2026-09-04 11:16:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
-const CACHE='cook-v2-2';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='cook-v2-7';
+
+const ASSETS=[
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys
+          .filter(key=>key!==CACHE)
+          .map(key=>caches.delete(key))
+      )
+    ).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request)
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      return cached || fetch(event.request);
+    })
+  );
+});
